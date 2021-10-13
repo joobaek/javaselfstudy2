eab1061e3bc128f6e035a9bc5aacb0ae9b9f7166
Jooyoung Baek <jyb1209@gmail.com>
2021-10-14 00:57:44 +0900

    chap2 commit

---
+++ src/sec01/exam01/VariableInitializationExample.java
@@ -0,0 +1,16 @@
+package sec01.exam01;
+
+public class VariableInitializationExample {
+
+	public static void main(String[] args) {
+
+		int value = 30;
+		
+		 int result = value + 10;
+		 
+		 System.out.println(result);
+		 
+		 
+	}
+
+}
