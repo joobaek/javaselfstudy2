b11d1b45f4d93ae3c2c2b5f6965f18b2fee8d531
Jooyoung Baek <jyb1209@gmail.com>
2021-10-18 21:55:39 +0900

    변수 타입2

---
+++ src/sec02/exam06/EscapeExample.java
@@ -0,0 +1,19 @@
+package sec02.exam06;
+
+public class EscapeExample {
+
+	public static void main(String[] args) {
+
+		System.out.println("번호\t이름\t직업");
+		System.out.print("행단위출력\n");
+		System.out.print("행단위출력");
+		System.out.print("우리는\"개발자\"입니다\n");
+		System.out.print("봄\\여름\\가을\\겨울");
+
+
+
+
+
+	}
+
+}
